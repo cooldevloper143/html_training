8fab4351d004977d66182a93644bd554bbbb9655
aisarthak <aisarthak2@gamil.com>
2023-10-15 21:39:58 +0530

    002

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-console.log("working proprely");
-function nameoffunction(a) {
-  console.log(a);
-  return console.log("the funciton is stoped execting");
-  console.log("this is ths proof ");
-}
-// a="hello"
-let a = "you have to achive your goal";
-nameoffunction(a);
-function multi(a, b, d) {
-  return (a * b) / d;
-}
-// multi(20,10);
-// const result=multi(20,10)
-console.log(multi(10, 10, 50));
-console.log(multi(500, 100, 2));
+console.log("the website is working properly")
