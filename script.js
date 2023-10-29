eccb6816ca17c14cddc2a096614662304a0a6c5b
aisarthak <aisarthak2@gamil.com>
2023-10-30 04:06:10 +0530

    212121

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("the website is working properly");
+let num = [3,4,5,6];
+
+for (let i = 0; i < num.length; i++) {
+    const element = array[i];
+    console.log(element);
+    
+}
+console.log("this website is working properly");
