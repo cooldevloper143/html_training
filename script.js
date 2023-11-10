90383150f2d4d7b8ea1e7f896b7baa24fabb2f4b
aisarthak <aisarthak2@gamil.com>
2023-11-10 09:28:55 +0530

    002

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-function handleclick(event) {
-  console.log("hello world");
+const person = {
+    firstname: "john",
+    lastname: "doe",
+    id : 5556,
+    fullname : function myfunction() {
+        return this.firstname + "  " + this.lastname;
+    },
+    secondfullname : `$firstname`+`$lastnaem`;
+    
 }
-function handleclick2(event) {
-  alert("welcome");
-}
-document.addEventListener("DOMContentLoaded", function () {
-    let date = document.getElementById("date")
-    date.addEventListener("click",
-        function (params) {
-            this.innerHTML = Date()
-        })
-  let button = document.getElementById("button");
-  button.addEventListener("click", handleclick);
-  button.addEventListener("mouseover", handleclick2);
-  const element = document.getElementById("div");
-  element.addEventListener("mouseover", function () {
-    element.style.backgroundColor = "blue";
-    element.style.borderRadius = "50px";
-});
-element.addEventListener("mouseout", function (params) {
-      element.style.backgroundColor = "black";
-      element.style.borderRadius = "50%";
-    })
-});
